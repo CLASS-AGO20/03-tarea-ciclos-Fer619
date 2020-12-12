e59f7c839e4794e512203ff4f93e0c59a7cdb657
Fer619 <fbriceno@ucol.mx>
2020-12-11 18:49:56 -0600

    implementation of sumatoriaSerieDos ()

--- src/main.js
+++ src/main.js
@@ -13,28 +13,38 @@ export default class App {
 
     sumatoriaSerieDos(numero){
 
+        let i= 1;
+        let suma= 0;
+
+        while(i<=numero){
+
+            if((i%2 ==0) || (i==1)){
+
+                suma= suma + (1/i);
+            }else{
+
+                suma= suma - (1/i);
+            }
+            i++;
+        }
+        return suma;
     }
 
     esPrimo(numero){
 
-        let i= 2;
+        let i= 2
         let primo= 0;
 
-        do {
+            if(numero%i ==0){
 
-            if(numero%i !==0){
+                return false;
 
-                primo= "true";
+            } else{
+                return true;
             }
-            i= 2;
         
-        }while(numero%i ===0){
-
-            primo= "false";
         }
 
-        return primo;
-    }
 
     
 
@@ -45,6 +55,8 @@ export default class App {
 let app = new App();
 
 console.log(app.sumatoriaSerieUno(4));
+
+console.log(app.sumatoriaSerieDos(5));
 
 console.log(app.esPrimo(7));
 console.log(app.esPrimo(10));
